c58b9c543e7f9deb5d0476c42957681d35491469
Louis Agyapong <47106199+louis-agyapong@users.noreply.github.com>
2023-03-17 17:07:51 +0000

    FastAPI docker

---
+++ src/main.py
@@ -0,0 +1,20 @@
+from typing import Union
+
+from fastapi import FastAPI
+import redis
+import debugpy
+
+r = redis.Redis(host="redis", port=6379)
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "Louis Agyapong"}
+
+
+@app.get("/hits")
+def read_root():
+    r.incr("hits")
+    return {"number of hits": r.get("hits")}
